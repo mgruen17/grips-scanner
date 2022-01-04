144a735416ac6baf55edb1942bf6dd30b0ba6f17
Michael Gruen <michael.gruen@posteo.de>
2022-01-04 11:57:01 +0100

    initial vimp download implementiation

--- crawl.py
+++ crawl.py
@@ -1,8 +1,34 @@
 import sys
 import requests
+from tqdm import tqdm
+from time import time
+import os
 from bs4 import BeautifulSoup
 
 session = requests.Session()
+invalid = '<>:"/\|?*'
+
+def download_file(link, path, filename = None):
+    URL_resource_get = link
+    resource_result = session.get(URL_resource_get, stream=True)
+    if filename == None:
+        filename = resource_result.url.split('/')[-1].split('?')[0]
+
+    for char in invalid:
+        filename = filename.replace(char, '_')
+    
+    fullpath = path + '/' + filename.strip()
+
+    print('Downloading "' + filename + '" to "' + path + '"')
+    if 'content-length' not in resource_result.headers:
+        total_size = 0
+    else:
+        total_size = int(resource_result.headers['content-length'])
+    chunk_size = 1024
+    with open(fullpath, "wb") as file:
+        for data in tqdm(iterable=resource_result.iter_content(chunk_size=chunk_size), total = total_size/chunk_size, unit='KB'):
+            file.write(data)
+    print('complete')
 
 if len(sys.argv) > 1:
     username = sys.argv[1]
@@ -12,17 +38,29 @@ else:
     username=input('enter username:')
     password=input('enter password:')
 
-URL_login_get = 'https://elearning.uni-regensburg.de/login/index.php'
-page_login_get = session.get(URL_login_get)
-soup = BeautifulSoup(page_login_get.text,'html.parser')
+URL_grips_login_get = 'https://elearning.uni-regensburg.de/login/index.php'
+page_grips_login_get = session.get(URL_grips_login_get)
+soup = BeautifulSoup(page_grips_login_get.text,'html.parser')
 token = soup.find_all(attrs={'name':'logintoken'})[0]['value']
 
-URL_login_post = 'https://elearning.uni-regensburg.de/login/index.php'
+URL_grips_login_post = 'https://elearning.uni-regensburg.de/login/index.php'
 files=dict(anchor='', logintoken=token, realm='hs', username=username, password=password)
 data={'anchor':'', 'logintoken':token, 'realm':'hs', 'username':username, 'password':password}
-page_login_post = session.post(URL_login_post, data=data)
-soup = BeautifulSoup(page_login_post.text,'html.parser')
-print(f'logged in as {soup.findAll("span","usertext")[0].text}\n\n')
+page_grips_login_post = session.post(URL_grips_login_post, data=data)
+soup = BeautifulSoup(page_grips_login_post.text,'html.parser')
+print(f'\nGRIPS: logged in as {soup.findAll("span","usertext")[0].text}')
+
+URL_vimp_login_get = 'https://vimp.oth-regensburg.de/login'
+page_vimp_login_get = session.get(URL_vimp_login_get)
+soup = BeautifulSoup(page_vimp_login_get.text,'html.parser')
+token = soup.find_all(attrs={'name':'signin[_csrf_token]'})[0]['value']
+
+URL_vimp_login_post = 'https://vimp.oth-regensburg.de/login'
+files=dict(anchor='', logintoken=token, realm='hs', username=username, password=password)
+data={'signin[username]': username, 'signin[password]': password, 'signin[position]': 'content', 'signin[_csrf_token]': token}
+page_vimp_login_post = session.post(URL_vimp_login_post, data=data)
+soup = BeautifulSoup(page_vimp_login_post.text,'html.parser')
+print(f'VIMP: logged in as {soup.select(".linkToProfile a")[0].text}\n\n')
 
 URL_dashboard_get = 'https://elearning.uni-regensburg.de/my'
 page_dashboard_get = session.get(URL_dashboard_get)
@@ -37,10 +75,36 @@ course_chosen_index = input('choose course:')
 course_chosen = courses[int(course_chosen_index)]
 print(f'\ncrawling {course_chosen.text}')
 
+timestamp = str(int(time()))
+course_directory = course_chosen.text.strip()
+for char in invalid:
+	course_directory = course_directory.replace(char, '_')
+path = 'Downloads/' + timestamp + '/' + course_directory
+os.makedirs(path,mode = 0o777, exist_ok=True)
+
 URL_course_get = course_chosen.contents[0]['href']
-print(URL_course_get)
-page_dashboard_get = session.get(URL_course_get)
-soup = BeautifulSoup(page_dashboard_get.text,'html.parser')
-resources = soup.select('li.activity.resource a')
-for resource in resources:
-    print(resource['href'])
+page_course_get = session.get(URL_course_get)
+soup = BeautifulSoup(page_course_get.text,'html.parser')
+
+# directly linked files
+# resources = soup.select('li.activity.resource a')
+# for resource in resources:
+#     download_file(resource['href'], path)
+
+# vimp links
+all_urls = soup.select('li.activity.url a')
+for url in all_urls:
+    url.span.span.decompose()
+    print(url.text)
+    url_result = session.get(url['href'])
+    url_soup = BeautifulSoup(url_result.text,'html.parser')
+    final_url = url_soup.select('.urlworkaround a')[0]['href']
+
+    # TODO: write to text file
+    print(final_url)
+
+    if final_url.startswith('https://vimp.oth-regensburg.de/'):
+        final_url_result = session.get(final_url)
+        final_url_soup = BeautifulSoup(final_url_result.text,'html.parser')
+        download_url = final_url_soup.select('meta[property="og:video:url"]')[0]['content']
+        download_file(download_url, path, url.text.strip() + '.' + download_url.split('.')[-1])
